068094f03ae649c4902aafdb66660561767015fe
unai5665 <unaiperez5665@gmail.com>
2024-03-19 12:18:45 +0100

    completado writer.py

--- writer.py
+++ writer.py
@@ -1,32 +1,35 @@
 class FileWriter:
-    def __init__(self, fileName):
-        self.fileName = fileName
+    def __init__(self, file_name):
+        self.file_name = file_name
         try:
-            self.i = open(self.fileName, "r+")
-        except Exception:
-            self.i = open(self.fileName, "w+")
+            self.file = open(self.file_name, "r+")  
+        except FileNotFoundError:  
+            self.file = open(self.file_name, "w+")  
 
-    def write(self, mensaje:str):
-        self.i.write(mensaje)
+    def write(self, message: str):
+        self.file.write(message)
+        self.file.flush()  
+    def read(self) -> str:
+        self.file.seek(0)  
+        return self.file.read()
 
-
-    def read(self)->str:
-        return self.i.readlines()
-
+    def readlines(self) -> list:
+        self.file.seek(0)  
+        return self.file.readlines()
 
     def close(self):
-        self.i.close()
+        self.file.close()
 
 
 if __name__ == '__main__':
     def main():
-        pruebaw = FileWriter("fileWriter.txt")
-        pruebaw.write("Hola")
+        file_writer = FileWriter("fileWriter.txt")
+        file_writer.write("cocacola\n")  
         
-        pruebaw.close()
+        file_writer.close()
         
-        pruebar = FileWriter("fileWriter.txt")
-        print(pruebar.read())
-        pruebar.close()
+        file_reader = FileWriter("fileWriter.txt")
+        print(file_reader.read())  
+        file_reader.close()
 
     main()
