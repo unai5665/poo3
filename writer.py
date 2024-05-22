2600afb8b7e4bb4c86e16105341df3414e717863
unai5665 <151141045+unai5665@users.noreply.github.com>
2024-05-22 06:49:13 +0100

    Update writer.py

--- writer.py
+++ writer.py
@@ -1,35 +1,20 @@
 class FileWriter:
     def __init__(self, file_name):
         self.file_name = file_name
-        try:
-            self.file = open(self.file_name, "r+")  
-        except FileNotFoundError:  
-            self.file = open(self.file_name, "w+")  
 
     def write(self, message: str):
-        self.file.write(message)
-        self.file.flush()  
+        with open(self.file_name, "w") as file:  
+            file.write(message)
+
     def read(self) -> str:
-        self.file.seek(0)  
-        return self.file.read()
-
-    def readlines(self) -> list:
-        self.file.seek(0)  
-        return self.file.readlines()
-
-    def close(self):
-        self.file.close()
-
+        with open(self.file_name, "r") as file: 
+            return file.read()
 
 if __name__ == '__main__':
     def main():
         file_writer = FileWriter("fileWriter.txt")
         file_writer.write("cocacola\n")  
         
-        file_writer.close()
-        
-        file_reader = FileWriter("fileWriter.txt")
-        print(file_reader.read())  
-        file_reader.close()
+        print(file_writer.read())  
 
     main()
